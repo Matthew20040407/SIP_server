9ccb738c2ea74b2060293e4a8e32199a9f4eba1e
Matthew20040407 <matthew@dhtsolution.group>
2026-03-06 15:38:23 +0800

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
-uv run ./receive_server.py & uv run call_center.py & wait
+cleanup() {
+    echo "Stopping servers..."
+    kill $RECEIVE_PID $CALLCENTER_PID 2>/dev/null
+    wait
+}
+trap cleanup EXIT INT TERM
 
+uv run ./receive_server.py &
+RECEIVE_PID=$!
+uv run call_center.py &
+CALLCENTER_PID=$!
+
+echo "receive_server.py PID: $RECEIVE_PID"
+echo "call_center.py PID: $CALLCENTER_PID"
 echo "All servers started"
+
+wait
